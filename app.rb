a3448aea7ee03c97e8c1333c748437a962cc20f9
Martina Radeva <martaradeva@yahoo.com>
2014-08-08 20:59:29 +0300

    sign bg working when link copied to another tab

--- app.rb
+++ app.rb
@@ -11,10 +11,11 @@ post "/s/" do
   @sign_text = params[:sign]
   @sign_colour = params[:colour]
   @sign_link = request.url + @sign_text
-  erb :sign_as_html
+  redirect to("/s/#{@sign_colour}/#{@sign_text}")
 end
 
-get "/s/:sign" do
+get "/s/:colour/:sign" do
   @sign_text = params[:sign]
+  @sign_colour = params[:colour]
   erb :sign_as_html
 end
