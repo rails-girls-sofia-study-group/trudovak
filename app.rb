7b1848c24bbfafb89311ea7ecac40854da1b62f7
Martina Radeva <martaradeva@yahoo.com>
2014-08-08 22:43:57 +0300

    special characters in sign text displayed properly
    URI.escape turns string into url.

--- app.rb
+++ app.rb
@@ -10,8 +10,10 @@ end
 post "/s/" do
   @sign_text = params[:sign]
   @sign_colour = params[:colour]
-  @sign_link = request.url + @sign_text
-  redirect to("/s/#{@sign_colour}/#{@sign_text}")
+  @sign_link = request.url + "/" + @sign_colour + "/" + URI.escape(@sign_text)
+  # redirect to("/s/#{@sign_colour}/#{URI.escape(@sign_text)}")
+  
+  redirect to(@sign_link)
 end
 
 get "/fullscreen/:colour/:sign" do
@@ -24,6 +26,6 @@ end
 get "/s/:colour/:sign" do
   @sign_text = params[:sign]
   @sign_colour = params[:colour]
-  @sign_link = request.url + @sign_text
+  @sign_link = request.url
   erb :sign_as_html
 end
