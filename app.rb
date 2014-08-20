83cb970e5303efa01bd36d5e11497e887feba5cc
Maria <mariai@capital.bg>
2014-08-20 22:31:22 +0300

    add choice of fonts

--- app.rb
+++ app.rb
@@ -10,22 +10,25 @@ end
 post "/s/" do
   @sign_text = params[:sign]
   @sign_colour = params[:colour]
-  @sign_link = request.url + "/" + @sign_colour + "/" + URI.escape(@sign_text)
+  @sign_font = params[:font]
+  @sign_link = request.url + @sign_colour + "/" + @sign_font  + "/" + URI.escape(@sign_text)
   # redirect to("/s/#{@sign_colour}/#{URI.escape(@sign_text)}")
   
   redirect to(@sign_link)
 end
 
-get "/fullscreen/:colour/:sign" do
+get "/fullscreen/:colour/:font/:sign" do
   @sign_text = params[:sign]
   @sign_colour = params[:colour]
+  @sign_font = params[:font]
   @sign_link = request.url + @sign_text
   erb :sign_as_html_fullscreen
 end
 
-get "/s/:colour/:sign" do
+get "/s/:colour/:font/:sign" do
   @sign_text = params[:sign]
   @sign_colour = params[:colour]
+  @sign_font = params[:font]
   @sign_link = request.url
   erb :sign_as_html
 end
