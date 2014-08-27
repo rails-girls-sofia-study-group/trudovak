d1ed9f01a79e383cb8f0d26981a0f523392c094d
Maria <mariai@capital.bg>
2014-08-27 21:36:20 +0300

    using CGI.escape to show question marks in tabelka

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 require 'rubygems'
 require 'bundler/setup'
+require 'cgi'
 Bundler.require(:default)
 
 get "/" do
@@ -11,22 +12,22 @@ post "/s/" do
   @sign_text = params[:sign]
   @sign_colour = params[:colour]
   @sign_font = params[:font]
-  @sign_link = request.url + @sign_colour + "/" + @sign_font  + "/" + URI.escape(@sign_text)
+  @sign_link = request.url + @sign_colour + "/" + @sign_font  + "/" + CGI.escape(@sign_text)
   # redirect to("/s/#{@sign_colour}/#{URI.escape(@sign_text)}")
   
   redirect to(@sign_link)
 end
 
 get "/fullscreen/:colour/:font/:sign" do
-  @sign_text = params[:sign]
+  @sign_text = CGI.unescape params[:sign]
   @sign_colour = params[:colour]
   @sign_font = params[:font]
-  @sign_link = request.url + @sign_text
+  @sign_link = request.url + CGI.escape(@sign_text)
   erb :sign_as_html_fullscreen
 end
 
 get "/s/:colour/:font/:sign" do
-  @sign_text = params[:sign]
+  @sign_text = CGI.unescape params[:sign]
   @sign_colour = params[:colour]
   @sign_font = params[:font]
   @sign_link = request.url
