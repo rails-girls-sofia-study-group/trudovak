3d9f8be0738fab999830137010ca7a9e25246745
Martina Radeva <martaradeva@yahoo.com>
2014-08-27 21:25:31 +0300

    form checkbox adds query parameter to url

--- app.rb
+++ app.rb
@@ -12,9 +12,15 @@ post "/s/" do
   @sign_colour = params[:colour]
   @sign_font = params[:font]
   @sign_link = request.url + @sign_colour + "/" + @sign_font  + "/" + URI.escape(@sign_text)
+  @flattened = params[:flattened]
+  puts @flattened.to_s
   # redirect to("/s/#{@sign_colour}/#{URI.escape(@sign_text)}")
-  
-  redirect to(@sign_link)
+  if (params[:flattened])
+    @sign_link_flattened = @sign_link + "?" + params[:flattened].to_s
+    # redirect to (@sign_link_flattened)
+  else
+    redirect to(@sign_link) 
+  end
 end
 
 get "/fullscreen/:colour/:font/:sign" do
@@ -32,3 +38,14 @@ get "/s/:colour/:font/:sign" do
   @sign_link = request.url
   erb :sign_as_html
 end
+
+# get "/s/:colour/:font/:sign?on" do
+#   @sign_text = params[:sign]
+#   @sign_colour = params[:colour]
+#   @sign_font = params[:font]
+#   @sign_link = request.url
+#   # erb :sign_as_html
+#   "WORKS"
+# end
+
+# submit?flattened=checked
