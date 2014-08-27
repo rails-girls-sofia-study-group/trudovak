48297e7b228364d1adfbf09c2c909e8b03d3c7e4
Martina Radeva <martaradeva@yahoo.com>
2014-08-27 21:49:15 +0300

    checkbox redirects user to "this will render image"

--- app.rb
+++ app.rb
@@ -14,11 +14,9 @@ post "/s/" do
   @sign_font = params[:font]
   @sign_link = request.url + @sign_colour + "/" + @sign_font  + "/" + CGI.escape(@sign_text)
   @flattened = params[:flattened]
-  puts @flattened.to_s
-  # redirect to("/s/#{@sign_colour}/#{URI.escape(@sign_text)}")
   if (params[:flattened])
-    @sign_link_flattened = @sign_link + "?" + params[:flattened].to_s
-    # redirect to (@sign_link_flattened)
+    @sign_link_flattened = @sign_link + "?flattened=" + params[:flattened].to_s
+    redirect to (@sign_link_flattened)
   else
     redirect to(@sign_link) 
   end
@@ -33,20 +31,13 @@ get "/fullscreen/:colour/:font/:sign" do
 end
 
 get "/s/:colour/:font/:sign" do
-  @sign_text = CGI.unescape params[:sign]
+  @sign_text = params[:sign]
   @sign_colour = params[:colour]
   @sign_font = params[:font]
   @sign_link = request.url
-  erb :sign_as_html
+  @flattened = params["flattened"]
+  if not @flattened
+    erb :sign_as_html
+  else "TODO: RENDER TEXT AS IMAGE"
+  end
 end
-
-# get "/s/:colour/:font/:sign?on" do
-#   @sign_text = params[:sign]
-#   @sign_colour = params[:colour]
-#   @sign_font = params[:font]
-#   @sign_link = request.url
-#   # erb :sign_as_html
-#   "WORKS"
-# end
-
-# submit?flattened=checked
